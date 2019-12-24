38662785d7a655679c514f6b6cf4213ae250a9a2
donghyeon-dev <donghaynzxc@gmail.com>
2019-12-24 17:43:01 +0900

    API만들기

---
+++ src/main/java/com/donghyeon/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.donghyeon.book.springboot.service.posts;
+
+import com.donghyeon.book.springboot.domain.posts.PostsRepository;
+import com.donghyeon.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
