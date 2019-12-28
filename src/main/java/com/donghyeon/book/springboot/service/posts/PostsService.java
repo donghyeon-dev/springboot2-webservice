d8b94e588b4d254b736946b83b32495b1763ce3f
donghyeon-dev <donghaynzxc@gmail.com>
2019-12-29 01:21:27 +0900

    Update 기능 추가(HttpMethod.PUT -> HttpMethod.POST)

--- src/main/java/com/donghyeon/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/donghyeon/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.donghyeon.book.springboot.service.posts;
 
+import com.donghyeon.book.springboot.domain.posts.Posts;
 import com.donghyeon.book.springboot.domain.posts.PostsRepository;
+import com.donghyeon.book.springboot.web.dto.PostsResponseDto;
 import com.donghyeon.book.springboot.web.dto.PostsSaveRequestDto;
+import com.donghyeon.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDTO){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        posts.update(requestDTO.getTitle(), requestDTO.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(
+                        ()-> new IllegalArgumentException("해당사용자가 없습니다. id ="+id)
+                );
+        return new PostsResponseDto(entity);
     }
 }
