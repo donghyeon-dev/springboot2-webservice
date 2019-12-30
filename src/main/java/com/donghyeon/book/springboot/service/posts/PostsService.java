eeb267b6e29c3ecc13c597fb375f727128b793be
donghyeon-dev <donghaynzxc@gmail.com>
2019-12-30 14:58:16 +0900

    mustache로 화면 구성, 게시글 수정 및 삭제 화면 만들기

--- src/main/java/com/donghyeon/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/donghyeon/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.donghyeon.book.springboot.service.posts;
 
 import com.donghyeon.book.springboot.domain.posts.Posts;
 import com.donghyeon.book.springboot.domain.posts.PostsRepository;
+import com.donghyeon.book.springboot.web.dto.PostsListResponseDto;
 import com.donghyeon.book.springboot.web.dto.PostsResponseDto;
 import com.donghyeon.book.springboot.web.dto.PostsSaveRequestDto;
 import com.donghyeon.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,13 +10,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
@@ -35,4 +39,19 @@ public class PostsService {
                 );
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+                                                        // = .map(posts -> new PostsListResponseDto(posts))
+    }
+
+    @Transactional
+    public void delete(Long id){
+
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당사용자가 없습니다. id="+id));
+        postsRepository.delete(posts);
+    }
+
 }
--- src/main/java/com/donghyeon/book/springboot/web/PostsApiController.java
+++ src/main/java/com/donghyeon/book/springboot/web/PostsApiController.java
@@ -18,13 +18,19 @@ public class PostsApiController {
 
         return postsService.save(requestDto);
     }
-    @PostMapping("/api/v1/posts/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDTO){
         return postsService.update(id,requestDTO);
     }
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 
 }
