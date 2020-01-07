199f22aaa9ac8b9a658ca1ad25190b269a78fbe7
donghyeon-dev <donghaynzxc@gmail.com>
2020-01-07 14:56:00 +0900

    어노테이션 기반으로 개선

---
+++ src/main/java/com/donghyeon/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,33 @@
+package com.donghyeon.book.springboot.config.auth;
+
+import com.donghyeon.book.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter){
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isUserClass;
+        //파라미터에 @LoginUser 어노테이션이 붙어있고, 파라미터 클래스 타입이 SessionUser.class 일 경우 true를 반환
+    }
+
+    @Override    //resoleArgument : 파라미터에 전달할 객체를 생성, 여기서는 세션에서 객체를 가져옴
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContatiner,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception{
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/com/donghyeon/book/springboot/web/IndexController.java
+++ src/main/java/com/donghyeon/book/springboot/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.donghyeon.book.springboot.web;
 
+import com.donghyeon.book.springboot.config.auth.LoginUser;
 import com.donghyeon.book.springboot.service.posts.PostsService;
 import com.donghyeon.book.springboot.web.dto.PostsResponseDto;
 import com.donghyeon.book.springboot.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
@@ -19,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName",user.getName());
         }
